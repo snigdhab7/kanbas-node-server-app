1e98bff3460128ab871fddbdfd38eee101c6b471
Snigdha Bose <62890614+snigdhab7@users.noreply.github.com>
2023-12-04 17:51:00 -0500

    a6

--- app.js
+++ app.js
@@ -6,18 +6,57 @@ import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import AssignmentRoutes from "./assignments/routes.js";
 import "dotenv/config";
+import mongoose from "mongoose";
+import UserRoutes from './users/routes.js';
+import session from "express-session";
+import "dotenv/config";
+
+//mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 
 
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    credentials: true,
+   //origin: "http://localhost:3000",
+   origin: process.env.FRONTEND_URL
+
+
+}));
+/* const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+  };
+  app.use(
+    session(sessionOptions)
+  ); */
+  
+  const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+  };
+  if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+    };
+  }
+  app.use(session(sessionOptions));
+   
 app.use(express.json());
 AssignmentRoutes(app);
 ModuleRoutes(app);
 CourseRoutes(app);
 Hello(app)
 Lab5(app);
+UserRoutes(app);
 app.listen(process.env.PORT || 4000);
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
+mongoose.connect(CONNECTION_STRING);
+
 
 /* const express = require('express')
 const app = express()
